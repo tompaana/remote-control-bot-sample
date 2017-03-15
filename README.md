162eba9ecc7f536ea55a329868f083c451ab676a
Tomi Paananen <tomi.paananen@microsoft.com>
2017-03-15 14:40:30 +0200

    Added two images for documentation, added some comments to NotificationsScorable.cs and updated README.md.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# remote-control-bot-sample
-This sample demonstrates how to control a chatbot, built on the Microsoft Bot Framework, utilizing backchannel messages.
+# Remote Control Bot Sample #
+
+This sample demonstrates how to control a chatbot, built on the
+[Microsoft Bot Framework](https://dev.botframework.com/), utilizing backchannel
+messages.
+
+![Sample in action](Documentation/Screenshot.png?raw=true)
+
+## Running and testing ##
+
+1. Clone or copy the repository
+2. Register a bot for this sample in [the bot portal](https://dev.botframework.com/)
+ * Unfortunately registering and publishing the bot is the easiest way to get
+   to test drive it, because the implementation is dependent on
+   [Direct Line](https://docs.botframework.com/en-us/restapi/directline3/)
+3. Get the **app ID** and **app password** (a step in registration phase) and
+   insert them into the [Web.config](RemoteControlBotSample/Web.config)
+4. Publish the bot (and insert the newly created endpoint to the bot portal)
+5. In the portal, activate Direct Line and get the first secret key
+6. Insert the secret key into [Program.cs](RemoteControlBotControllerSample/Program.cs)
+7. Say something to the bot so that it knows who you are (your virtual address),
+   you can do this on any channel the bot is on or using emulator
+ * If you don't say anything the bot won't know you and cannot notify you
+8. Run the console app (RemoteControlBotControllerSample)
+9. Enjoy your three notifications
+
+## See also ##
+
+* [Remotely Controlled Bots article](http://tomipaananen.azurewebsites.net/?p=2231)
--- RemoteControlBotSample/Notifications/NotificationsScorable.cs
+++ RemoteControlBotSample/Notifications/NotificationsScorable.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace RemoteControlBotSample.Notifications
 {
+    /// <summary>
+    /// A class that integrates to the Microsoft Bot Framework and looks for notification related
+    /// backchannel messages from incoming activities.
+    /// </summary>
     public class NotificationsScorable : ScorableBase<IActivity, string, double>
     {
         private const string NotificationBackchannelId = "notification";
@@ -22,21 +26,38 @@ namespace RemoteControlBotSample.Notifications
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Returns the score (match of the activity to types this class handles).
+        /// 1 means we are going to consume the activity. 0 score means we don't care about
+        /// the current activity.
+        /// </summary>
+        /// <param name="activity"></param>
+        /// <param name="state">Will contain the notification data, if the activity contained it.
+        /// An empty string otherwise.</param>
+        /// <returns></returns>
         protected override double GetScore(IActivity activity, string state)
         {
             return (string.IsNullOrEmpty(state) ? 0d : 1d);
         }
 
+        /// <summary>
+        /// Tells us if we this scorable has reached a score or not.
+        /// </summary>
+        /// <param name="activity"></param>
+        /// <param name="state">Will contain the notification data, if the activity contained it.
+        /// An empty string otherwise.</param>
+        /// <returns></returns>
         protected override bool HasScore(IActivity activity, string state)
         {
             return !string.IsNullOrEmpty(state);
         }
 
         /// <summary>
-        /// 
+        /// Sends the notification(s).
         /// </summary>
         /// <param name="activity"></param>
-        /// <param name="state">Should contain the notification as JSON string. We store it to the state in PrepareAsync method (see the return value).</param>
+        /// <param name="state">Should contain the notification as JSON string.
+        /// We store it to the state in PrepareAsync method (see the return value).</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         protected override async Task PostAsync(IActivity activity, string state, CancellationToken cancellationToken)
@@ -45,6 +66,13 @@ namespace RemoteControlBotSample.Notifications
             await _dialogTask.PollAsync(cancellationToken);
         }
 
+        /// <summary>
+        /// Checks the given activity for a backchannel message ordering the bot to send out
+        /// notifications.
+        /// </summary>
+        /// <param name="activity">The received activity.</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>The notification data as JSON string, if found. An empty string otherwise.</returns>
 #pragma warning disable 1998
         protected override async Task<string> PrepareAsync(IActivity activity, CancellationToken cancellationToken)
         {
